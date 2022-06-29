d0a100779ebe13b48e9c7a098b338c4173bb6b22
AngelMond <abraham_will@hotmail.com>
2022-06-29 16:03:54 -0500

    Created route when an endpoint doesn't exists

--- controllers/index.js
+++ controllers/index.js
@@ -5,4 +5,11 @@ const apiRoutes = require('./api')
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
+//If a route doesn't exists respond this
+router.use('*', async (req, res) => {
+    res.status(500).json({
+        success: false,
+        message: 'endpoint does not exist!'
+    })
+});
 module.exports = router;
