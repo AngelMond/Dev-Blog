3b4c0a1f6c7942dbdb54ed7285de1e1f0e88d8c8
AngelMond <abraham_will@hotmail.com>
2022-07-05 21:54:07 -0500

    Updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -4,12 +4,9 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize( 
 'mysql://b2551357e488f1:6b55890c@us-cdbr-east-06.cleardb.net/heroku_b177fec47b1d76a?reconnect=true', {
-      host: 'us-cdbr-east-06.cleardb.net',
+      
       dialect: 'mysql',
       port: 3306,
-      dialectOptions: {
-        decimalNumbers: true,
-      },
     });
 
 module.exports = sequelize;
