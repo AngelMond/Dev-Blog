60cdd79d9c76dd74a8cd9d5f5722e6feb5876f18
AngelMond <abraham_will@hotmail.com>
2022-07-05 21:49:57 -0500

    Updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize( 
+'mysql://b2551357e488f1:6b55890c@us-cdbr-east-06.cleardb.net/heroku_b177fec47b1d76a?reconnect=true', {
       host: 'us-cdbr-east-06.cleardb.net',
       dialect: 'mysql',
+      port: 3306,
       dialectOptions: {
         decimalNumbers: true,
       },
